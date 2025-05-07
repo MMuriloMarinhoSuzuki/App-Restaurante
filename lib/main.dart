be7067611d538ca322dae99c9cd3dc04b7332ef8
Murilo <Murisuzuki11@gmail.com>
2025-05-06 22:41:41 -0300

    s

--- lib/main.dart
+++ lib/main.dart
@@ -1,51 +1,104 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MaterialApp(home: Restaurante(),));
+  runApp(MaterialApp(home: Restaurant()));
 }
-class Restaurante extends StatelessWidget {
-  const Restaurante({super.key});
+
+class Restaurant extends StatefulWidget {
+  const Restaurant({super.key});
+
+  @override
+  State<Restaurant> createState() => _Restaurant();
+}
+
+class _Restaurant extends State<Restaurant> {
+    int cont = 0;
+    String msg = "Pode entrar";
+  void increment(){
+      if(cont < 10){
+        setState(() {
+          cont++;
+        });
+      }else {
+        msg = "lotado!";
+      } 
+    }
+  }
+
+  void decrement(){
+      setState() {
+        if(cont > 0){
+              cont--;
+        }  
+    };
+  }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home:Scaffold( 
+    return  Scaffold(
         backgroundColor: const Color.fromARGB(255, 0, 0, 0),
-         body: Container(
-            decoration: BoxDecoration(image: DecorationImage(image: AssetImage("imagem/restaurant.png"),
-            fit:BoxFit.cover),
+        body: Container(
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage("imagem/restaurant.png"),
+              fit: BoxFit.cover,
             ),
-          child:  Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Text("PODE ENTRAR!!!", style: TextStyle(fontSize: 30, color: const Color.fromARGB(255, 101, 101, 101) ),),
-          Text("0", style: TextStyle(fontSize: 100, color: const Color.fromARGB(255, 255, 255, 255) ),),
-          
-          Row(
+          ),
+          child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-            TextButton(onPressed:null , child: Text("Entrou"),
-            style: TextButton.styleFrom(
-            backgroundColor: Colors.white,
-            fixedSize: Size(100, 100), 
-            shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(24) ),
-            ),
-            ),
-            SizedBox(width: 20,),
-            TextButton(onPressed:null , child: Text("Saiu"),
-            style: TextButton.styleFrom(
-            backgroundColor: Colors.white,
-            fixedSize: Size(100, 100), 
-            shape: RoundedRectangleBorder(borderRadius:BorderRadius.circular(24) ),
-            ),
-            ),
-          ],),
+              Text(
+                cont == 10 ? "Lotado": "pode entrar",
+                style: TextStyle(
+                  fontSize: 30,
+                  color: const Color.fromARGB(255, 101, 101, 101),
+                ),
+              ),
+              Text(
+                cont.toString() ,
+                style: TextStyle(
+                  fontSize: 100,
+                  color: const Color.fromARGB(255, 255, 255, 255),
+                ),
+              ),
 
-          ],)
-         )
-
-    
-      ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextButton(
+                    onPressed: increment,
+                    child: Text("Entrou"),
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      fixedSize: Size(100, 100),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 20),
+                  TextButton(
+                    onPressed: decrement,
+                    child: Text("Saiu"),
+                    style: TextButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      fixedSize: Size(100, 100),
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(24),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
     );
   }
+
 }
+
+ 
+
+
+
